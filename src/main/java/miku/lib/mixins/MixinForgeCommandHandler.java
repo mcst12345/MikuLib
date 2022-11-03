3dfda961d05d31e3090b1622f76c14c7d337e9c4
mcst12345 <herobrinetlb@gmail.com>
2022-11-03 21:51:14 +0800

    Update

--- src/main/java/miku/lib/mixins/MixinForgeCommandHandler.java
+++ src/main/java/miku/lib/mixins/MixinForgeCommandHandler.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = FMLCommonHandler.class)
+@Mixin(value = FMLCommonHandler.class,remap = false)
 public class MixinForgeCommandHandler {
     @Inject(at=@At("HEAD"),method = "exitJava", cancellable = true)
     public void exitJava(int exitCode, boolean hardExit, CallbackInfo ci){
