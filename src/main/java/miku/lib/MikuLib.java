59a2ff19a7ccb4b3421f61ee4d39145ed073ef03
mcst12345 <herobrinetlb@gmail.com>
2022-12-18 17:17:22 +0800

    Update

--- src/main/java/miku/lib/MikuLib.java
+++ src/main/java/miku/lib/MikuLib.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.Logger;
 public class MikuLib {
     public static final String MODID = "mikulib";
     public static final String NAME = "MikuLib";
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.5";
 
     public MikuLib() {
     }
