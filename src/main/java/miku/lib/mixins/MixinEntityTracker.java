2323ee68e6225ce86f7ef68b45bcacd39be8cab0
root <herobrinetlb@gmail.com>
2023-06-20 16:03:14 +0800

    Commit2023/5/27

--- src/main/java/miku/lib/mixins/MixinEntityTracker.java
+++ src/main/java/miku/lib/mixins/MixinEntityTracker.java
@@ -1,6 +1,6 @@
 package miku.lib.mixins;
 
-import miku.Items.Miku.MikuItem;
+import miku.lib.item.SpecialItem;
 import miku.lib.util.EntityUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityTracker;
@@ -14,20 +14,20 @@ public class MixinEntityTracker {
 
     @Inject(at=@At("HEAD"),method = "track(Lnet/minecraft/entity/Entity;IIZ)V",cancellable = true)
     public void track(Entity entityIn, int trackingRange, final int updateFrequency, boolean sendVelocityUpdates, CallbackInfo ci){
-        if(MikuItem.isTimeStop() || EntityUtil.isKilling()){
+        if(SpecialItem.isTimeStop() || EntityUtil.isKilling()){
             if(!EntityUtil.isProtected(entityIn))ci.cancel();
         }
     }
 
     @Inject(at=@At("HEAD"),method = "track(Lnet/minecraft/entity/Entity;)V",cancellable = true)
     public void track(Entity entityIn, CallbackInfo ci){
-        if(MikuItem.isTimeStop() || EntityUtil.isKilling()){
+        if(SpecialItem.isTimeStop() || EntityUtil.isKilling()){
             if(!EntityUtil.isProtected(entityIn))ci.cancel();
         }
     }
 
     @Inject(at=@At("HEAD"),method = "tick",cancellable = true)
     public void tick(CallbackInfo ci){
-        if(MikuItem.isTimeStop() || EntityUtil.isKilling())ci.cancel();
+        if(SpecialItem.isTimeStop() || EntityUtil.isKilling())ci.cancel();
     }
 }
