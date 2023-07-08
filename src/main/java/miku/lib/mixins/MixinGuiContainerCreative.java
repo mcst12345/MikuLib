5077c21f0ba18dc4fe75367386b15c050d79df6b
root <herobrinetlb@gmail.com>
2023-07-08 13:00:23 +0800

    Commit2023/7/6

--- src/main/java/miku/lib/mixins/MixinGuiContainerCreative.java
+++ src/main/java/miku/lib/mixins/MixinGuiContainerCreative.java
@@ -1,7 +1,9 @@
 package miku.lib.mixins;
 
 import net.minecraft.client.gui.inventory.GuiContainerCreative;
+import net.minecraft.client.renderer.InventoryEffectRenderer;
 import net.minecraft.inventory.ClickType;
+import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,9 +11,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = GuiContainerCreative.class)
-public class MixinGuiContainerCreative {
+public abstract class MixinGuiContainerCreative extends InventoryEffectRenderer {
+    public MixinGuiContainerCreative(Container inventorySlotsIn) {
+        super(inventorySlotsIn);
+    }
+
     @Inject(at=@At("HEAD"),method = "handleMouseClick", cancellable = true)
     protected void handleMouseClick(Slot slotIn, int slotId, int mouseButton, ClickType type, CallbackInfo ci){
-        if(slotIn.getStack() == null)ci.cancel();
+        if(this.mc.player.inventory.getItemStack() == null)ci.cancel();
     }
 }
