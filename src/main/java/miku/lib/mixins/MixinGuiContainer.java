a134add7dbdf64b91c7f31db0f0695b1df26e6a4
root <herobrinetlb@gmail.com>
2023-07-08 12:47:55 +0800

    Commit2023/7/6

--- src/main/java/miku/lib/mixins/MixinGuiContainer.java
+++ src/main/java/miku/lib/mixins/MixinGuiContainer.java
@@ -1,5 +1,6 @@
 package miku.lib.mixins;
 
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,10 +9,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = GuiContainer.class)
-public class MixinGuiContainer {
+public abstract class MixinGuiContainer extends GuiScreen {
 
     @Inject(at=@At("HEAD"),method = "drawSlot", cancellable = true)
     private void drawSlot(Slot slotIn, CallbackInfo ci){
-        if(slotIn.getStack().getItem() == null)ci.cancel();
+        if(slotIn.getStack() == null)ci.cancel();
     }
 }
