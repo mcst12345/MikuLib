460410148777a6be51d755babeeb07efd6809717
root <herobrinetlb@gmail.com>
2023-07-09 17:17:37 +0800

    Commit2023/7/8

--- src/main/java/miku/lib/MikuLib.java
+++ src/main/java/miku/lib/MikuLib.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 public class MikuLib {
     public static final String MODID = "mikulib";
     public static final String NAME = "MikuLib";
-    public static final String VERSION = "1.6.10";
+    public static final String VERSION = "1.6.11";
 
     public MikuLib() {
     }
