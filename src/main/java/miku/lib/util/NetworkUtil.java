8093dfe6d583a693e2ced1cad2f561b75a791d52
root <herobrinetlb@gmail.com>
2023-07-09 12:31:36 +0800

    Commit2023/7/8

--- src/main/java/miku/lib/util/NetworkUtil.java
+++ src/main/java/miku/lib/util/NetworkUtil.java
@@ -1,6 +1,7 @@
 package miku.lib.util;
 
 import com.anotherstar.network.LoliKillEntityPacket;
+import miku.lib.sqlite.Sqlite;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraftforge.fml.common.Loader;
@@ -8,6 +9,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 
 public class NetworkUtil {
     public static boolean isBadPacket(IMessage message){
+        if((boolean) Sqlite.GetValueFromTable("debug","CONFIG",0))return false;
         if (Loader.isModLoaded("lolipickaxe")) {
             if (message instanceof LoliKillEntityPacket) {
                 Entity entity = Minecraft.getMinecraft().player.world.getEntityByID(((LoliKillEntityPacket) message).getEntityID());
