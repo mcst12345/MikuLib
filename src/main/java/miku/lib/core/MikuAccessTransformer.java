7f7e54bff1679a3ecea71b50394931d68adac15e
root <herobrinetlb@gmail.com>
2023-07-10 21:16:36 +0800

    Commit2023/7/10

--- src/main/java/miku/lib/core/MikuAccessTransformer.java
+++ src/main/java/miku/lib/core/MikuAccessTransformer.java
@@ -1,11 +1,27 @@
 package miku.lib.core;
 
 import net.minecraft.launchwrapper.IClassTransformer;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 public class MikuAccessTransformer implements IClassTransformer {
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
         if(!transformedName.matches("net.minecraft.(.*)") && !transformedName.matches("net.minecraftforge.(.*)")){
+
+            ClassReader cr = new ClassReader(basicClass);
+            ClassNode cn = new ClassNode();
+            cr.accept(cn, 0);
+
+            for (MethodNode mn : cn.methods){
+
+            }
 
         }
         return basicClass;
