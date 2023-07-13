3a1e3aff1f0fa9924b8bb701538529d7a129c377
root <herobrinetlb@gmail.com>
2023-07-13 19:23:10 +0800

    Commit2023/7/11

--- src/main/java/miku/lib/mixins/minecraft/MixinGuiMainMenu.java
+++ src/main/java/miku/lib/mixins/minecraft/MixinGuiMainMenu.java
@@ -1,7 +1,10 @@
 package miku.lib.mixins.minecraft;
 
+import com.google.common.collect.Lists;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.IResource;
 import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,21 +14,55 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
+
 @Mixin(value = GuiMainMenu.class)
 public abstract class MixinGuiMainMenu extends GuiScreen {
-    @Mutable
-    @Shadow @Final private static ResourceLocation SPLASH_TEXTS;
 
     @Mutable
     @Shadow @Final private static ResourceLocation field_194400_H;
 
-    @Inject(at=@At("HEAD"),method = "drawPanorama")
-    public void init(int mouseX, int mouseY, float partialTicks,CallbackInfo ci){
-        if(!SPLASH_TEXTS.getNamespace().equals("miku"))SPLASH_TEXTS = new ResourceLocation("miku:texts/splashes.txt");
+    @Shadow private String splashText;
+    @Shadow @Final private static Random RANDOM;
+    private static final ResourceLocation MikuText = new ResourceLocation("miku:texts/splashes.txt");
+
+    @Inject(at=@At("TAIL"),method = "<init>")
+    public void init(CallbackInfo ci){
+        try {
+            List<String> list = Lists.newArrayList();
+            IResource iresource = Minecraft.getMinecraft().getResourceManager().getResource(MikuText);
+            BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(iresource.getInputStream(), StandardCharsets.UTF_8));
+            String s;
+
+            while ((s = bufferedreader.readLine()) != null) {
+                s = s.trim();
+
+                if (!s.isEmpty()) {
+                    list.add(s);
+                }
+            }
+
+            if (!list.isEmpty())
+            {
+
+                do {
+                    this.splashText = list.get(RANDOM.nextInt(list.size()));
+
+                } while (this.splashText.hashCode() == 125780783);//What the fuck is this number?
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @Inject(at=@At("HEAD"),method = "drawScreen")
     public void drawScreen(int mouseX, int mouseY, float partialTicks, CallbackInfo ci){
-        if(!field_194400_H.getNamespace().equals("miku"))field_194400_H = new ResourceLocation("miku:textures/gui/Miku-Edition.png");
+        if(!field_194400_H.getNamespace().equals("miku"))field_194400_H = new ResourceLocation("miku:textures/gui/miku-edition.png");
     }
 }
