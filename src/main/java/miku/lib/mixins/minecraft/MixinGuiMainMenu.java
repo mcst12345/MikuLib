bf1846e2a6004eda44331e8a7ea08a19e5cee3b5
root <herobrinetlb@gmail.com>
2023-07-13 19:10:23 +0800

    Commit2023/7/11

--- src/main/java/miku/lib/mixins/minecraft/MixinGuiMainMenu.java
+++ src/main/java/miku/lib/mixins/minecraft/MixinGuiMainMenu.java
@@ -16,8 +16,16 @@ public abstract class MixinGuiMainMenu extends GuiScreen {
     @Mutable
     @Shadow @Final private static ResourceLocation SPLASH_TEXTS;
 
-    @Inject(at=@At("TAIL"),method = "<init>")
-    public void init(CallbackInfo ci){
-        SPLASH_TEXTS = new ResourceLocation("miku:texts/splashes.txt");
+    @Mutable
+    @Shadow @Final private static ResourceLocation field_194400_H;
+
+    @Inject(at=@At("HEAD"),method = "drawPanorama")
+    public void init(int mouseX, int mouseY, float partialTicks,CallbackInfo ci){
+        if(!SPLASH_TEXTS.getNamespace().equals("miku"))SPLASH_TEXTS = new ResourceLocation("miku:texts/splashes.txt");
+    }
+
+    @Inject(at=@At("HEAD"),method = "drawScreen")
+    public void drawScreen(int mouseX, int mouseY, float partialTicks, CallbackInfo ci){
+        if(!field_194400_H.getNamespace().equals("miku"))field_194400_H = new ResourceLocation("miku:textures/gui/Miku-Edition.png");
     }
 }
