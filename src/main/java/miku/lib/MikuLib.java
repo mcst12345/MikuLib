8c6b6d36e9789dad3f69a70be035ec42623dee0b
root <herobrinetlb@gmail.com>
2023-07-16 18:01:15 +0800

    1.7.10

--- src/main/java/miku/lib/MikuLib.java
+++ src/main/java/miku/lib/MikuLib.java
@@ -5,6 +5,6 @@ import cpw.mods.fml.common.Mod;
 @Mod(modid = "mikulib",version = "1.0",name = "MikuLib")
 public class MikuLib {
     public MikuLib(){
-
+        System.out.println("MikuLib is loading...");
     }
 }
