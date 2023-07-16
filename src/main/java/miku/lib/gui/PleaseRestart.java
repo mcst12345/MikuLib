19f256d38c202a68af51789525ce79dbb6ec1a31
root <herobrinetlb@gmail.com>
2023-07-16 20:37:14 +0800

    Commit2023/7/14

--- src/main/java/miku/lib/gui/PleaseRestart.java
+++ src/main/java/miku/lib/gui/PleaseRestart.java
@@ -5,10 +5,11 @@ import java.awt.*;
 
 public class PleaseRestart extends JPanel {
     public PleaseRestart(){
+        this.setLocation(0,0);
         JTextArea jt2=new JTextArea();   //new一个文本区
         JFrame jf=new JFrame("Please restart.");
         jf.setVisible(true);                     //窗体可见
-        jf.setSize(100, 100);      //窗体大小
+        jf.setSize(300, 300);      //窗体大小
         jf.setLayout(new BorderLayout());    //边界布局
         jt2.setBackground(Color.WHITE);
         jt2.setText("MikuLib has just extracted the sqlite module of it. Please restart it.");
