f1e1521d8393490598fc0b173c6ca01b7a3545ea
root <herobrinetlb@gmail.com>
2023-07-16 22:38:45 +0800

    Commit2023/7/14

--- src/main/java/miku/lib/gui/PleaseRestart.java
+++ src/main/java/miku/lib/gui/PleaseRestart.java
@@ -8,6 +8,7 @@ public class PleaseRestart extends JPanel {
         this.setLocation(0,0);
         JTextArea jt2=new JTextArea();   //new一个文本区
         JFrame jf=new JFrame("Please restart.");
+        Container container = jf.getContentPane();
         jf.setVisible(true);                     //窗体可见
         jf.setSize(300, 300);      //窗体大小
         jf.setLayout(new BorderLayout());    //边界布局
