546cf18c6bb865776315275059acbb4be15014a8
root <herobrinetlb@gmail.com>
2023-07-19 21:46:21 +0800

    Commit2023/7/19

--- src/main/java/miku/lib/thread/TransformerFucker.java
+++ src/main/java/miku/lib/thread/TransformerFucker.java
@@ -6,9 +6,14 @@ import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.Launch;
 
 import java.lang.reflect.Field;
+import java.util.Iterator;
 import java.util.List;
 
 public class TransformerFucker extends Thread {
+    private static boolean isGoodTransformer(String s) {
+        return s.matches("(.*)optifine(.*)") || s.matches("(.*)minecraft(.*)");
+    }
+
     @Override
     public void run() {
         while (true) {
@@ -17,7 +22,13 @@ public class TransformerFucker extends Thread {
                 Field transformers = Launch.classLoader.getClass().getDeclaredField("transformers");
                 transformers.setAccessible(true);
                 List<IClassTransformer> t = (List<IClassTransformer>) transformers.get(Launch.classLoader);
-                t.removeIf(transformer -> !ASMUtil.isGoodClass(transformer.getClass().toString().substring(5).trim()));//Fuck other transformers.
+                //t.removeIf(transformer -> !ASMUtil.isGoodClass(transformer.getClass().toString().substring(5).trim()));//Fuck other transformers.
+                Iterator<IClassTransformer> iterator = t.iterator();
+                while (iterator.hasNext()) {
+                    IClassTransformer classTransformer = iterator.next();
+                    if (!ASMUtil.isGoodClass(classTransformer.getClass().toString().substring(5).trim()) && !isGoodTransformer(classTransformer.getClass().toString().substring(5).trim()))
+                        iterator.remove();
+                }
                 if (!(t instanceof MikuArrayListEarly)) {
                     MikuArrayListEarly<IClassTransformer> fucked = new MikuArrayListEarly<IClassTransformer>(2);
                     for (IClassTransformer i : t) {
