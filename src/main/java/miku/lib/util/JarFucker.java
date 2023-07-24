fa59a44749859992ae00d04332003f3da6420833
root <herobrinetlb@gmail.com>
2023-07-24 10:28:36 +0800

    commit

--- src/main/java/miku/lib/util/JarFucker.java
+++ src/main/java/miku/lib/util/JarFucker.java
@@ -2,19 +2,26 @@ package miku.lib.util;
 
 
 import miku.lib.core.MikuTransformer;
+import miku.lib.util.transform.ASMUtil;
+import miku.lib.util.transform.MixinUtil;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.*;
 import sun.misc.IOUtils;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Enumeration;
+import java.util.Objects;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
+
+import static miku.lib.core.MikuTransformer.*;
+import static miku.lib.sqlite.Sqlite.DEBUG;
 
 public class JarFucker {
     protected static final IClassTransformer Miku = new MikuTransformer();
@@ -64,7 +71,7 @@ public class JarFucker {
                             }
                         }
                         if (shouldAdd) {
-                            byte[] fucked = Miku.transform(null, cn.name.replace("/", "."), original);
+                            byte[] fucked = transform(cn.name.replace("/", "."), cn);
                             if (fucked != original) changed = true;
                             jos.putNextEntry(new JarEntry(entry.getName()));
                             jos.write(fucked);
@@ -127,5 +134,112 @@ public class JarFucker {
 
     public static boolean shouldRestart() {
         return shouldRestart;
+    }
+
+    protected static final byte[] transform(String transformedName, ClassNode cn) {
+        System.out.println("Examine class:" + transformedName);
+
+        cached_methods.clear();
+        double num = 0.0d;
+
+        if (DEBUG()) {
+            print("Class name:" + cn.name);
+            print("Class sign:" + cn.signature);
+            print("outer class:" + cn.outerClass);
+            print("outer method:" + cn.outerMethod);
+            print("outer method desc:" + cn.outerMethodDesc);
+            System.out.println("Interfaces:");
+            for (String s : cn.interfaces) {
+                print(s);
+            }
+        }
+        if (cn.visibleAnnotations != null) {
+            System.out.println("visibleAnnotations:");
+            for (AnnotationNode an : cn.visibleAnnotations) {
+                if (DEBUG()) {
+                    print(an.desc);
+                    if (an.values != null) print(an.values.toString());
+                }
+            }
+        }
+
+        if (cn.visibleTypeAnnotations != null) for (TypeAnnotationNode an : cn.visibleTypeAnnotations) {
+            {
+                System.out.println("visibleTypeAnnotations:");
+                if (DEBUG()) {
+                    print(an.desc);
+                    if (an.values != null) print(an.values.toString());
+                }
+            }
+        }
+
+        if (cn.invisibleAnnotations != null) {
+            System.out.println("invisibleAnnotations:");
+            for (AnnotationNode an : cn.invisibleAnnotations) {
+                if (DEBUG()) {
+                    print(an.desc);
+                    if (an.values != null) print(an.values.toString());
+                }
+                if (Objects.equals(an.desc, "Lorg/spongepowered/asm/mixin/Mixin;")) {
+                    System.out.println("Found mixin class:" + cn.name + ",fucking it.");
+                    MixinUtil.FuckMixinClass(cn);
+                    ClassWriter cw = new ClassWriter(0);
+                    cn.accept(cw);
+                    return cw.toByteArray();
+                }
+            }
+        }
+
+        if (cn.invisibleTypeAnnotations != null) {
+            System.out.println("invisibleTypeAnnotations:");
+            for (TypeAnnotationNode an : cn.invisibleTypeAnnotations) {
+                if (DEBUG()) {
+                    print(an.desc);
+                    print(an.values.toString());
+                }
+            }
+        }
+
+        double tmp = cn.methods.size();
+
+
+        if (ASMUtil.isBadClass(transformedName)) {
+            System.out.println("Find dangerous class " + cn.name + ",fucking it.");
+            ASMUtil.FuckClass(cn);
+            ClassWriter cw = new ClassWriter(0);
+            cn.accept(cw);
+            return cw.toByteArray();
+        }
+
+
+        cn.methods.removeIf(mn -> ASMUtil.isBadMethod(mn, cn.name));
+
+
+        double possibility = num / tmp;
+        System.out.println("The danger-value of class " + cn.name + ":" + possibility);
+
+        if (possibility > 0.6d) {
+            System.out.println(cn.name + "is too dangerous. Destroy it.");
+            ASMUtil.FuckClass(cn);
+            ClassWriter cw = new ClassWriter(0);
+            cn.accept(cw);
+            return cw.toByteArray();
+        } else if (possibility > 0.4d) {
+            System.out.println(cn.name + "contains too many dangerous methods.Fucking those methods.");
+            for (MethodNode m : cached_methods) {
+                cn.methods.remove(m);
+            }
+        }
+
+        for (FieldNode field : cn.fields) {
+            if (ASMUtil.isBadField(field)) BadFields.add(field);
+        }
+
+
+        ClassWriter cw = new ClassWriter(0);
+
+        cn.accept(cw);
+
+        return cw.toByteArray();
     }
 }
