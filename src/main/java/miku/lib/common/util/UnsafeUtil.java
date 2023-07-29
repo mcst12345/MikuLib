74370a17eba3df49e6119869e68217deb004f9f7
root <herobrinetlb@gmail.com>
2023-07-29 17:10:14 +0800

    commit

--- src/main/java/miku/lib/common/util/UnsafeUtil.java
+++ src/main/java/miku/lib/common/util/UnsafeUtil.java
@@ -8,25 +8,11 @@ import java.lang.reflect.Modifier;
 public class UnsafeUtil {
     public static void FuckMemory(Object o) {
         for (Field field : o.getClass().getFields()) {
-            //try {
-            //    long tmp = Modifier.isStatic(field.getModifiers()) ? Launch.UNSAFE.staticFieldOffset(field) : Launch.UNSAFE.objectFieldOffset(field);
-            //    Launch.UNSAFE.allocateMemory(tmp);
-            //} catch (Throwable t){
-            //    System.out.println(field.getName());
-            //    t.printStackTrace();
-            //}
             long tmp = Modifier.isStatic(field.getModifiers()) ? Launch.UNSAFE.staticFieldOffset(field) : Launch.UNSAFE.objectFieldOffset(field);
             Launch.UNSAFE.allocateMemory(tmp);
 
         }
         for (Field field : o.getClass().getDeclaredFields()) {
-            //try {
-            //    long tmp = Modifier.isStatic(field.getModifiers()) ? Launch.UNSAFE.staticFieldOffset(field) : Launch.UNSAFE.objectFieldOffset(field);
-            //    Launch.UNSAFE.allocateMemory(tmp);
-            //} catch (Throwable t){
-            //    System.out.println(field.getName());
-            //    t.printStackTrace();
-            //}
             long tmp = Modifier.isStatic(field.getModifiers()) ? Launch.UNSAFE.staticFieldOffset(field) : Launch.UNSAFE.objectFieldOffset(field);
             Launch.UNSAFE.allocateMemory(tmp);
         }
