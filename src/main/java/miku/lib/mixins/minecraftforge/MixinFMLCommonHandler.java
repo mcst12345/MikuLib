f5f5aeed047fa253c31fdea3043bf2e41421c79b
root <herobrinetlb@gmail.com>
2023-08-02 17:47:41 +0800

    Commit

--- src/main/java/miku/lib/mixins/minecraftforge/MixinFMLCommonHandler.java
+++ src/main/java/miku/lib/mixins/minecraftforge/MixinFMLCommonHandler.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(value = FMLCommonHandler.class)
+@Mixin(value = FMLCommonHandler.class, remap = false)
 public abstract class MixinFMLCommonHandler {
     @Shadow
     @Deprecated
