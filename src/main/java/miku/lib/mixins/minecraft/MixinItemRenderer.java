557c97064f8ff099bc50b9c481819a89c2298523
root <herobrinetlb@gmail.com>
2023-08-03 11:14:33 +0800

    Commit

--- src/main/java/miku/lib/mixins/minecraft/MixinItemRenderer.java
+++ src/main/java/miku/lib/mixins/minecraft/MixinItemRenderer.java
@@ -1,8 +1,57 @@
 package miku.lib.mixins.minecraft;
 
+import miku.lib.client.api.iMinecraft;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.MapData;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = ItemRenderer.class)
 public class MixinItemRenderer {
+    @Shadow
+    @Final
+    private Minecraft mc;
+
+    @Shadow
+    @Final
+    private static ResourceLocation RES_MAP_BACKGROUND;
+
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    private void renderMapFirstPerson(ItemStack stack) {
+        GlStateManager.rotate(180.0F, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotate(180.0F, 0.0F, 0.0F, 1.0F);
+        GlStateManager.scale(0.38F, 0.38F, 0.38F);
+        GlStateManager.disableLighting();
+        this.mc.getTextureManager().bindTexture(RES_MAP_BACKGROUND);
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        GlStateManager.translate(-0.5F, -0.5F, 0.0F);
+        GlStateManager.scale(0.0078125F, 0.0078125F, 0.0078125F);
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+        bufferbuilder.pos(-7.0D, 135.0D, 0.0D).tex(0.0D, 1.0D).endVertex();
+        bufferbuilder.pos(135.0D, 135.0D, 0.0D).tex(1.0D, 1.0D).endVertex();
+        bufferbuilder.pos(135.0D, -7.0D, 0.0D).tex(1.0D, 0.0D).endVertex();
+        bufferbuilder.pos(-7.0D, -7.0D, 0.0D).tex(0.0D, 0.0D).endVertex();
+        tessellator.draw();
+        MapData mapdata = ((net.minecraft.item.ItemMap) stack.getItem()).getMapData(stack, this.mc.world);
+
+        if (mapdata != null) {
+            ((iMinecraft) this.mc).MikuEntityRenderer().getMapItemRenderer().renderMap(mapdata, false);
+        }
+
+        GlStateManager.enableLighting();
+    }
 }
