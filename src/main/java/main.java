316638d987bd7a794cd157a00f00f8b0e7845c81
root <herobrinetlb@gmail.com>
2023-08-06 12:49:46 +0800

    Commit

--- src/main/java/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        String c = "net.minecraftforge.fml.common.asm.transformers.SideTransformer";
-        String s = "$wrapper.net.minecraftforge.fml.common.asm.transformers.SideTransformer";
-        System.out.println(s.contains(c));
-    }
-}
