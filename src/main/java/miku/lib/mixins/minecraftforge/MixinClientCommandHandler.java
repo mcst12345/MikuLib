55e009a5c07c9c86c094f2d8124fee024d75eda2
root <herobrinetlb@gmail.com>
2023-08-09 17:52:56 +0800

    Commit

---
+++ src/main/java/miku/lib/mixins/minecraftforge/MixinClientCommandHandler.java
@@ -0,0 +1,71 @@
+package miku.lib.mixins.minecraftforge;
+
+import miku.lib.common.core.MikuLib;
+import net.minecraft.command.*;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.client.IClientCommand;
+import net.minecraftforge.event.CommandEvent;
+import net.minecraftforge.fml.common.FMLLog;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import static net.minecraft.util.text.TextFormatting.RED;
+
+@Mixin(value = ClientCommandHandler.class)
+public abstract class MixinClientCommandHandler extends CommandHandler {
+    @Shadow
+    protected abstract TextComponentTranslation format(TextFormatting color, String str, Object... args);
+
+    /**
+     * @author mcst12345
+     * @reason Shit!
+     */
+    @Overwrite
+    public int executeCommand(ICommandSender sender, String message) {
+        message = message.trim();
+
+        boolean usedSlash = message.startsWith("/");
+        if (usedSlash) {
+            message = message.substring(1);
+        }
+
+        String[] temp = message.split(" ");
+        String[] args = new String[temp.length - 1];
+        String commandName = temp[0];
+        System.arraycopy(temp, 1, args, 0, args.length);
+        ICommand icommand = getCommands().get(commandName);
+
+        try {
+            if (icommand == null || (!usedSlash && icommand instanceof IClientCommand && !((IClientCommand) icommand).allowUsageWithoutPrefix(sender, message))) {
+                return 0;
+            }
+
+            if (icommand.checkPermission(this.getServer(), sender)) {
+                CommandEvent event = new CommandEvent(icommand, sender, args);
+                if (MikuLib.MikuEventBus().post(event)) {
+                    if (event.getException() != null) {
+                        throw event.getException();
+                    }
+                    return 0;
+                }
+
+                this.tryExecute(sender, args, icommand, message);
+                return 1;
+            } else {
+                sender.sendMessage(format(RED, "commands.generic.permission"));
+            }
+        } catch (WrongUsageException wue) {
+            sender.sendMessage(format(RED, "commands.generic.usage", format(RED, wue.getMessage(), wue.getErrorObjects())));
+        } catch (CommandException ce) {
+            sender.sendMessage(format(RED, ce.getMessage(), ce.getErrorObjects()));
+        } catch (Throwable t) {
+            sender.sendMessage(format(RED, "commands.generic.exception"));
+            FMLLog.log.error("Command '{}' threw an exception:", message, t);
+        }
+
+        return -1;
+    }
+}
--- src/main/java/miku/lib/mixins/minecraftforge/MixinForgeHooksClient.java
+++ src/main/java/miku/lib/mixins/minecraftforge/MixinForgeHooksClient.java
@@ -3,19 +3,39 @@ package miku.lib.mixins.minecraftforge;
 import miku.lib.common.command.MikuInsaneMode;
 import miku.lib.common.core.MikuLib;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.SoundManager;
+import net.minecraft.client.gui.BossInfoClient;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ModelManager;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.MovementInput;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.registry.IRegistry;
 import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.event.*;
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
+import net.minecraftforge.client.model.ModelDynBucket;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.model.animation.Animation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.BOSSINFO;
 
 @Mixin(value = ForgeHooksClient.class, remap = false)
 public class MixinForgeHooksClient {
@@ -170,5 +190,116 @@ public class MixinForgeHooksClient {
             System.out.println("MikuWarn:Catch exception at GuiScreenEvent.DrawScreenEvent.Post");
             t.printStackTrace();
         }
+    }
+
+    /**
+     * @author mcst12345
+     * @reason FUCK!
+     */
+    @Overwrite
+    public static void onTextureStitchedPre(TextureMap map) {
+        MikuLib.MikuEventBus().post(new TextureStitchEvent.Pre(map));
+        ModelLoader.White.INSTANCE.register(map);
+        ModelDynBucket.LoaderDynBucket.INSTANCE.register(map);
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static void onTextureStitchedPost(TextureMap map) {
+        MikuLib.MikuEventBus().post(new TextureStitchEvent.Post(map));
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static void onBlockColorsInit(BlockColors blockColors) {
+        MikuLib.MikuEventBus().post(new ColorHandlerEvent.Block(blockColors));
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static void onItemColorsInit(ItemColors itemColors, BlockColors blockColors) {
+        MikuLib.MikuEventBus().post(new ColorHandlerEvent.Item(itemColors, blockColors));
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static ISound playSound(SoundManager manager, ISound sound) {
+        PlaySoundEvent e = new PlaySoundEvent(manager, sound);
+        MikuLib.MikuEventBus().post(e);
+        return e.getResultSound();
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static float getFogDensity(EntityRenderer renderer, Entity entity, IBlockState state, float partial, float density) {
+        EntityViewRenderEvent.FogDensity event = new EntityViewRenderEvent.FogDensity(renderer, entity, state, partial, density);
+        if (MikuLib.MikuEventBus().post(event)) return event.getDensity();
+        return -1;
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static void onFogRender(EntityRenderer renderer, Entity entity, IBlockState state, float partial, int mode, float distance) {
+        MikuLib.MikuEventBus().post(new EntityViewRenderEvent.RenderFogEvent(renderer, entity, state, partial, mode, distance));
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static void onModelBake(ModelManager modelManager, IRegistry<ModelResourceLocation, IBakedModel> modelRegistry, ModelLoader modelLoader) {
+        MikuLib.MikuEventBus().post(new ModelBakeEvent(modelManager, modelRegistry, modelLoader));
+        modelLoader.onPostBakeEvent(modelRegistry);
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static RenderGameOverlayEvent.BossInfo bossBarRenderPre(ScaledResolution res, BossInfoClient bossInfo, int x, int y, int increment) {
+        RenderGameOverlayEvent.BossInfo evt = new RenderGameOverlayEvent.BossInfo(new RenderGameOverlayEvent(Animation.getPartialTickTime(), res),
+                BOSSINFO, bossInfo, x, y, increment);
+        MikuLib.MikuEventBus().post(evt);
+        return evt;
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static void bossBarRenderPost(ScaledResolution res) {
+        MikuLib.MikuEventBus().post(new RenderGameOverlayEvent.Post(new RenderGameOverlayEvent(Animation.getPartialTickTime(), res), BOSSINFO));
+    }
+
+    /**
+     * @author mcst12345
+     * @reason Fuck
+     */
+    @Overwrite
+    public static ScreenshotEvent onScreenshot(BufferedImage image, File screenshotFile) {
+        ScreenshotEvent event = new ScreenshotEvent(image, screenshotFile);
+        MikuLib.MikuEventBus().post(event);
+        return event;
     }
 }
