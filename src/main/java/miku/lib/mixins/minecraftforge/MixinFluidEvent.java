98ad1ae52521635862f46d555edd0fc095366d96
root <herobrinetlb@gmail.com>
2023-08-10 14:02:44 +0800

    Commit

--- src/main/java/miku/lib/mixins/minecraftforge/MixinFluidEvent.java
+++ src/main/java/miku/lib/mixins/minecraftforge/MixinFluidEvent.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.eventhandler.Event;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(value = FluidEvent.class)
+@Mixin(value = FluidEvent.class, remap = false)
 public abstract class MixinFluidEvent extends Event {
     /**
      * @author mcst12345
