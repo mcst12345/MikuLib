1b9d6671a1ffb873decd3401012653110ab7fb90
root <herobrinetlb@gmail.com>
2023-08-27 15:32:08 +0800

    Commit

--- src/main/java/miku/lib/common/core/LibLoader.java
+++ src/main/java/miku/lib/common/core/LibLoader.java
@@ -1,21 +1,15 @@
 package miku.lib.common.core;
 
-import miku.lib.client.model.ModelLain;
-import miku.lib.client.render.RenderLain;
 import miku.lib.common.entity.Lain;
 import miku.lib.common.entity.MikuArrow;
 import miku.lib.common.entity.MikuTNT;
 import miku.lib.common.util.Register;
 import miku.lib.common.world.Void;
 import net.minecraft.world.biome.Biome;
-import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.EntityEntry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class LibLoader {
@@ -30,12 +24,5 @@ public class LibLoader {
     @SubscribeEvent
     public void onRegisterBiomeEvent(RegistryEvent.Register<Biome> event) {
         event.getRegistry().register(Void.VoidBiome.setRegistryName("miku:void"));
-    }
-
-    @SubscribeEvent
-    @SideOnly(Side.CLIENT)
-    public void RegisterModel(ModelRegistryEvent event) {
-        System.out.println("MikuInfo:Registering renders.");
-        RenderingRegistry.registerEntityRenderingHandler(Lain.class, manager -> new RenderLain(manager, new ModelLain(), 0.0f));
     }
 }
--- src/main/java/miku/lib/common/core/proxy/ClientProxy.java
+++ src/main/java/miku/lib/common/core/proxy/ClientProxy.java
@@ -1,7 +1,11 @@
 package miku.lib.common.core.proxy;
 
 import miku.lib.client.event.KeyBoardEvent;
+import miku.lib.client.model.ModelLain;
+import miku.lib.client.render.RenderLain;
+import miku.lib.common.entity.Lain;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
@@ -9,10 +13,13 @@ public class ClientProxy extends CommonProxy{
     public ClientProxy(){}
 
     @Override
-    public void preInit(FMLPreInitializationEvent event){
+    public void preInit(FMLPreInitializationEvent event) {
         super.preInit(event);
+        System.out.println("MikuInfo:Registering Keys.");
         KeyBoardEvent.Init();//Register key bindings.
         MinecraftForge.EVENT_BUS.register(new KeyBoardEvent());//Register keyboard event.
+        System.out.println("MikuInfo:Registering renders.");
+        RenderingRegistry.registerEntityRenderingHandler(Lain.class, manager -> new RenderLain(manager, new ModelLain(), 0.0f));
     }
 
     @Override
