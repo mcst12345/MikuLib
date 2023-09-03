4c67ac7b98d92c96f1b608c6069b0cbe6c386cb6
root <herobrinetlb@gmail.com>
2023-09-03 15:58:41 +0800

    Commit

--- src/main/java/miku/lib/mixins/minecraft/MixinChunkProviderServer.java
+++ src/main/java/miku/lib/mixins/minecraft/MixinChunkProviderServer.java
@@ -4,11 +4,14 @@ import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import it.unimi.dsi.fastutil.longs.LongArrayList;
 import miku.lib.common.api.iAnvilChunkLoader;
 import miku.lib.common.api.iChunkProviderServer;
+import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.IChunkLoader;
 import net.minecraft.world.gen.ChunkProviderServer;
+import net.minecraft.world.gen.IChunkGenerator;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.List;
@@ -33,8 +36,18 @@ public class MixinChunkProviderServer implements iChunkProviderServer {
     @Final
     public IChunkLoader chunkLoader;
 
+    @Mutable
+    @Shadow
+    @Final
+    public IChunkGenerator chunkGenerator;
+
+    @Shadow
+    @Final
+    public WorldServer world;
+
     @Override
     public void reload() {
+        this.chunkGenerator = this.world.worldInfo.getTerrainType().getChunkGenerator(world, world.worldInfo.getGeneratorOptions());
         List<Long> cache = new LongArrayList();
         cache.addAll(loadedChunks.keySet());
         this.loadedChunks.clear();
